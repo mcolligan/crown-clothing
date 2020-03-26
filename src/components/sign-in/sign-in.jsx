554902138ce46d1f9fb0ba7e081e173af4efc8bb
mcolligan <mcolligan1@gmail.com>
2020-03-26 13:29:43 -0600

    signin ready

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.jsx';
+import CustomButton from '../custom-button/custom-button.jsx';
+import './sign-in.scss';
+
+export default class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: '', password: '' });
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required/>
+
+          <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
+
+          <CustomButton type="submit"> Sign In </CustomButton>
+        </form>
+      </div>
+    )
+  }
+}
--- src/pages/sign-in-up/sign-in-up.jsx
+++ src/pages/sign-in-up/sign-in-up.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import './sign-in-up.scss';
+import SignIn from '../../components/sign-in/sign-in.jsx';
 
 const SignInAndUp = () => (
   <div className="sign-in-and-sign-up">
-
+    <SignIn />
   </div>
 );
 
