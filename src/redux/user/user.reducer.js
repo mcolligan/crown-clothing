9b14844958b68bd92b45a6c162b5cd4e34a867a4
mcolligan <mcolligan1@gmail.com>
2020-04-27 11:36:37 -0600

    reducers

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+    return {
+      ...state, //spread in rest of state, change current user below
+      currentUser: action.payload
+    }
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
