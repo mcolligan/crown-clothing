fb0b4e95111e77500f7f7bae1fd6ba106faa5b14
mcolligan <mcolligan1@gmail.com>
2020-04-29 17:10:17 -0600

    updates to cart

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -7,8 +7,8 @@ const PreviewCollection = ({ title, items }) => (
   <div className="collection-preview">
     <h1>{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.filter((item, idx) => idx < 4).map(({ id, ...itemProps}) => (
-        <CollectionItem key={id} {...itemProps} />
+      {items.filter((item, idx) => idx < 4).map(item => (
+        <CollectionItem key={item.id} item={item} />
       ))}
     </div>
   </div>
