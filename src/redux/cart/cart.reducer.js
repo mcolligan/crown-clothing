349359639fd5a6f4e516f2d333aa4220dd50c7a9
mcolligan <mcolligan1@gmail.com>
2020-05-05 13:03:12 -0600

    add/remove carrots added to checkout page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types.js';
-import { addItemToCart } from './cart.utils.js';
+import { addItemToCart, removeItemFromCart } from './cart.utils.js';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -24,6 +24,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: state.cartItems.filter(
           (cartItem) => cartItem.id !== action.payload.id
         ),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     default:
       return state;
