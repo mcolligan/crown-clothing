cfc7a4e0dac3b27bc54972816394446947baf913
mcolligan <mcolligan1@gmail.com>
2020-05-12 12:22:52 -0600

    fully functioning site minus stripe

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -6,21 +6,25 @@ import { addItem } from '../../redux/cart/cart.actions.js';
 
 import './collection-item.scss';
 
-const CollectionItem = ({ item, addItem  }) => {
-  const {  name, price, imageUrl } = item;
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+    <div className='collection-item'>
+      <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        {' '}
+        Add to Cart{' '}
+      </CustomButton>
     </div>
-    <CustomButton onClick={() => addItem(item)} inverted> Add to Cart </CustomButton>
-  </div>
-)};
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  addItem: item => dispatch(addItem(item))
+  addItem: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = (
+  { match } //match, location and histroy passed with Route from app
+) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
